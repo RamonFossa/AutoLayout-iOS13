d0b8366b5833a55b0cc5e3e58bf8e1a2ce678c37
RamonSynoro <ramon.fossa@triibo.com.br>
2023-07-19 21:38:33 -0300

    fix: Trying to fix button click, created a shake feature

--- AutoLayout-iOS13/ViewController.swift
+++ AutoLayout-iOS13/ViewController.swift
@@ -13,14 +13,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
-        let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
-        
-        diceImageView1.image = allDice[Int.random(in: 0...5)]
-        diceImageView2.image = allDice[Int.random(in: 0...5)]
-        
+    
+    let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    func rollDice() {
+        diceImageView1.image = self.allDice[Int.random(in: 0...5)]
+        diceImageView2.image = self.allDice[Int.random(in: 0...5)]
     }
     
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        self.rollDice()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+            if(event?.subtype == UIEvent.EventSubtype.motionShake) {
+                self.rollDice()
+            }
+        }
 }
 
